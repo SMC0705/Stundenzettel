10c2e6f088dc99789f79d70a5808144473991fdd
SMC0705 <germany.player1@gmail.com>
2026-09-19 08:55:22 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+/* Service Worker: macht die App offline nutzbar */
+const CACHE = "stundenzettel-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./jspdf.umd.min.js",
+                "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
